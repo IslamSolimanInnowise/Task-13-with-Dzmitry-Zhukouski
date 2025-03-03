5dcc15014dcb1e6c5823b2a98f05e12eed61621c
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-03-03 10:19:42 +0300

    init: start the application

---
+++ src/shared/styles/globalStyles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*,
+*::before,
+*::after{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body{
+    font-family: sans-serif;
+    background-color: ${({ theme }) => theme.backgroundColor};
+    color: ${({ theme }) => theme.color};
+    transition: 0.3s;
+    min-height: 100vh;
+}
+`;
