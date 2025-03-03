5dc7184856934fd307bae4baec9c7e8b64998395
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-03-03 16:52:35 +0300

    fix: change chakra directory to shared/ui

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ import prettier from 'eslint-config-prettier';
 import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'src/shared/ui'] },
   {
     extends: [
       js.configs.recommended,
