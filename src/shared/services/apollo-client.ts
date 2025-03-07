7855117e51a25b89422d959631928ab781f84996
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-03-07 11:40:42 +0300

    refactor: add handleUnauthorizedError function

--- src/shared/services/apollo-client.ts
+++ src/shared/services/apollo-client.ts
@@ -11,23 +11,27 @@ import { authVar } from '@shared/store/globalAuthState';
 
 import updateAccessToken from './updateAccessToken';
 
+const handleUnauthorizedError = async () => {
+  if (authVar().refreshToken !== null) {
+    const newToken = await updateAccessToken();
+
+    if (newToken) {
+      localStorage.setItem('access-token', newToken);
+
+      authVar({
+        accessToken: newToken,
+        refreshToken: authVar().refreshToken,
+        id: authVar().id,
+      });
+    }
+  }
+};
+
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) {
     graphQLErrors.forEach(async ({ message }) => {
       if (message === 'Unauthorized') {
-        if (authVar().refreshToken !== null) {
-          const newToken = await updateAccessToken();
-
-          if (newToken) {
-            localStorage.setItem('access-token', newToken);
-
-            authVar({
-              accessToken: newToken,
-              refreshToken: authVar().refreshToken,
-              id: authVar().id,
-            });
-          }
-        }
+        await handleUnauthorizedError();
       } else {
         notify({
           type: 'error',
