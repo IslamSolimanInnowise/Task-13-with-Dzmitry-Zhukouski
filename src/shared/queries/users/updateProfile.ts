0eea86ec3d62d856b221b1fa2c671a2cf35990b2
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-03-13 10:18:49 +0300

    fix: fix the immediate firstname and lastname update issue

--- src/shared/queries/users/updateProfile.ts
+++ src/shared/queries/users/updateProfile.ts
@@ -3,8 +3,10 @@ import { gql } from '@apollo/client';
 export const UPDATE_PROFILE = gql`
   mutation UpdateProfile($profile: UpdateProfileInput!) {
     updateProfile(profile: $profile) {
+      id
       first_name
       last_name
+      avatar
     }
   }
 `;
