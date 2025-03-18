5d7a6d7c83f2472271480c0b71995617a5831152
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-03-18 14:25:44 +0300

    fix: remove login notification from skills and languages

--- src/features/hooks/users/useGetUser.ts
+++ src/features/hooks/users/useGetUser.ts
@@ -5,12 +5,6 @@ import { GET_USER } from '@shared/queries/users/getUser';
 const useGetUser = (userId: string) => {
   return useQuery(GET_USER, {
     variables: { userId },
-    onCompleted: () => {
-      notify({
-        type: 'success',
-        title: 'Login successful',
-      });
-    },
   });
 };
 
