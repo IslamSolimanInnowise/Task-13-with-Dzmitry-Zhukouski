6a6fdfd21fc9474a991e5a5008aa6baf111874d9
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-03-20 12:42:18 +0300

    feat: add breadcrumb component

--- README.md
+++ README.md
@@ -6,22 +6,8 @@ Status of Last Deployment:
 
 todo:
 
-- add language schema => done
-- add react-hook-form setup => done
-- add form fields to add a language => done
-- add a language mutation => done
-- useAddLanguage hook => done
-- submit the form and finish the query => done
-- handle Closing the modal => done
-
-## rendering the languages
-
-- similar to the Skill Component, will need a language one => done
-
-## delete language mutation
-
-- write the mutation => done
-- write the hook => done
-- add the delete button function in the Language Component => done
-
-## update language mutation => done
+- breadcrumb for the profile pages
+- routes of the profile pages
+- make the skills page
+- make the languages page
+- add profile page with adding avatar and edit profile capabilities
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,16 +1,30 @@
 import Aside from '@entities/ui/Aside';
+import useGetUser from '@features/hooks/users/useGetUser';
 
-import { StyledPageContainer } from './profile.styles';
+import { StyledPageContainer, StyledPageContent } from './profile.styles';
+import Breadcrumb from '@entities/ui/Breadcrumb';
 
 interface ProfilePageProps {
   userId: string;
 }
 
 const ProfilePage: React.FC<ProfilePageProps> = ({ userId }) => {
+  const { data: currentUser, loading } = useGetUser(userId);
+  console.log(currentUser);
+  const profileCurrentLink =
+    currentUser?.user?.profile?.first_name || currentUser?.user?.email;
+
   return (
     <StyledPageContainer>
       <Aside />
-      <span>Profile: {userId}</span>
+      {!loading && (
+        <StyledPageContent>
+          <Breadcrumb
+            currentLink={profileCurrentLink}
+            breadCrumbItems={[{ name: 'Employees', path: '/users' }]}
+          />
+        </StyledPageContent>
+      )}
     </StyledPageContainer>
   );
 };
