85e0a6ac797ed279136044c963ac34742b98a80f
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-03-24 11:36:43 +0300

    feat: add notifications translations

--- src/features/hooks/users/useDeleteAvatar.ts
+++ src/features/hooks/users/useDeleteAvatar.ts
@@ -3,15 +3,17 @@ import { notify } from '@shared/Notifications/notify';
 import { DELETE_AVATAR } from '@shared/queries/users/deleteAvatar';
 import { GET_USER } from '@shared/queries/users/getUser';
 import { authVar } from '@shared/store/globalAuthState';
+import { useTranslation } from 'react-i18next';
 
 const useDeleteAvatar = () => {
+  const { t } = useTranslation('users');
   const { id } = authVar();
 
   return useMutation(DELETE_AVATAR, {
     onCompleted: () => {
       notify({
         type: 'success',
-        title: 'Your avatar has been Deleted',
+        title: t('usersNotifications.useDeleteAvatar.success'),
       });
     },
     refetchQueries: [{ query: GET_USER, variables: { userId: id } }],
--- src/features/hooks/users/useUploadAvatar.ts
+++ src/features/hooks/users/useUploadAvatar.ts
@@ -3,15 +3,17 @@ import { notify } from '@shared/Notifications/notify';
 import { GET_USER } from '@shared/queries/users/getUser';
 import { UPLOAD_AVATAR } from '@shared/queries/users/uploadAvatar';
 import { authVar } from '@shared/store/globalAuthState';
+import { useTranslation } from 'react-i18next';
 
 const useUploadAvatar = () => {
+  const { t } = useTranslation('users');
   const { id } = authVar();
 
   return useMutation(UPLOAD_AVATAR, {
     onCompleted: () => {
       notify({
         type: 'success',
-        title: 'Your avatar has been updated',
+        title: t('usersNotifications.useUploadAvatar.success'),
       });
     },
     refetchQueries: [{ query: GET_USER, variables: { userId: id } }],
